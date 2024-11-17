f5bd76926b0a9c78126c5f815a39e50cafc607af
kraskoval <kr.muratovski@gmail.com>
2024-11-17 16:33:22 +0200

    Dy 51: Transfer the data from the form to SQLIte db.

--- app.py
+++ app.py
@@ -1,9 +1,44 @@
-from flask import Flask, render_template
+from datetime import datetime
 
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "<KEY>"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Form(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	first_name = db.Column(db.String(80))
+	last_name = db.Column(db.String(80))
+	email = db.Column(db.String(80))
+	available_date = db.Column(db.Date)
+	occupation = db.Column(db.String(80))
+@app.route('/', methods = ["GET", "POST"])
 def index():
+	print(request.method)
+	if request.method == "POST":
+		first_name = request.form['first_name']
+		last_name = request.form['last_name']
+		email = request.form['email']
+		date = request.form['date']
+		date_obj = datetime.strptime(date, "%Y-%m-%d")
+		occupation = request.form['occupation']
+		print(first_name, last_name, email, date, occupation)
+
+		form = Form(first_name=first_name, last_name=last_name,
+					email=email, available_date=date_obj, occupation = occupation)
+		db.session.add(form)
+		db.session.commit()
+
+
+
 	return render_template("index.html")
+
+if __name__ == "__main__":
+	with app.app_context():
+		db.create_all()
+		app.run(debug=True)
 
 app.run(debug=True, port=5001)
